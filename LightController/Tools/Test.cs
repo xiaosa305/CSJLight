62f49d8801488790aca4fab340079aa64632d8cf
xiaosa <callmexiaosa@gmail.com>
2019-07-02 16:10:25 +0800

    添加通信功能模块测试类

---
+++ LightController/Tools/Test.cs
@@ -0,0 +1,19 @@
+﻿using LightController.Ast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightController.Tools
+{
+    /// <summary>
+    /// 测试用
+    /// </summary>
+    class Test
+    {
+        public Test(DBWrapper dBWrapper)
+        {
+
+        }
+    }
+}

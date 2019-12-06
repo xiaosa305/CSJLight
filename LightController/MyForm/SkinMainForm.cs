db864720cee7c9544212cfa4df5c898f5cfd5afc
Dickov <fwm164@qq.com>
2019-12-06 16:46:27 +0800

    ..

--- LightController/MyForm/SkinMainForm.cs
+++ LightController/MyForm/SkinMainForm.cs
@@ -2406,7 +2406,7 @@ namespace LightController.MyForm
 			int buttonIndex = MathAst.GetIndexNum(((Button)sender).Name, 0);
 			Console.WriteLine(buttonIndex);
 			//Test test = new Test(GetDBWrapper(true) , valueDAO, globalIniPath);
-			Test test = new Test(GetDBWrapper(false) );
+			Test test = new Test(GetDBWrapper(true) );
 			test.Start(buttonIndex);
 		}
 

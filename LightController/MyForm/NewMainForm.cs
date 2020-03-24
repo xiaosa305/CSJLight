b01547c113fce93d89dadb586f4645dd4bd61c61
Dickov <fwm164@qq.com>
2020-03-24 15:04:26 +0800

    《灯控》-去掉一进去就打开灯库编辑的测试代码；

--- LightController/MyForm/NewMainForm.cs
+++ LightController/MyForm/NewMainForm.cs
@@ -233,11 +233,9 @@ namespace LightController.MyForm
 
 		private void NewMainForm_Load(object sender, EventArgs e)
 		{
-			// 启动时刷新可用串口列表;
+			//MARK：思考启动时是否刷新可用串口列表;
 			//refreshComList();	
 
-			//TODO：20-03-24 为方便调试，将软件一打开 就启用《灯库编辑》,后期要删掉
-			openLightEditor();
 		}
 
 		/// <summary>

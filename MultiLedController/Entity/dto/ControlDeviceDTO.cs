21cb7299a45f6ae8a2d47b3ee8f6f99bff566f69
xiaosa <callmexiaosa@gmail.com>
2020-06-04 09:24:48 +0800

    +++添加幻彩控制器包装类 +++添加新增幻彩控制器设备方法重载

---
+++ MultiLedController/Entity/dto/ControlDeviceDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiLedController.entity.dto
+{
+    public class ControlDeviceDTO
+    {
+        public ControlDevice ControlDevice { get; set; }
+        public List<String> VirtualIps { get; set; }
+        public ControlDeviceDTO(ControlDevice controlDevice,List<string> virtualIps)
+        {
+            this.ControlDevice = controlDevice;
+            this.VirtualIps = virtualIps;
+        }
+    }
+}

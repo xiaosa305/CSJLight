4d2e57b89dcbd01c5d2b14d65751c7ea7517594f
xiaosa <callmexiaosa@gmail.com>
2020-07-08 09:32:43 +0800

    ++++添加大数据整理测试接口

--- LightController/Tools/XiaosaTest.cs
+++ LightController/Tools/XiaosaTest.cs
@@ -32,100 +32,21 @@ namespace LightController.Tools
 
         public void NewConnectTest(DBWrapper wrapper,string configPath)
         {
-            //if (this.Connect == null)
-            //{
-            //    //this.Connect = new NetworkConnect(new NetworkDeviceInfo() { DeviceAddr = 1, DeviceIp = "192.168.31.153", DeviceName = "大房101" });
-            //}
-            //if (this.Connect.IsConnected())
-            //{
-            //    //this.Connect.DownloadProject(wrapper, configPath, DownloadCompleted, DownloadError, DownloadProgress);
-            //    //this.Connect.PutParam(@"C:\Users\99729\Dev\Gitee\CSJLight\LightController\HardwareSet.ini", DownloadCompleted, DownloadError);
-            //    //this.Connect.GetParam(DownloadCompleted, DownloadError);
-            //}
+            
         }
-
-        public void DownloadCompleted(Object obj,string msg)
-        {
-            CSJ_Hardware hardware = obj as CSJ_Hardware;
-            Console.WriteLine("下载成功[" + msg + "]");
-        }
-        public void DownloadError(string msg)
-        {
-            Console.WriteLine("下载失败[" + msg + "]");
-        }
-        public  void DownloadProgress(string fileName,int progress)
-        {
-            Console.WriteLine(fileName + "下载进度：" + progress);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         public void OpenSerialPort()
         {
-            NetworkDeviceInfo info = new NetworkDeviceInfo();
-            info.DeviceIp = "192.168.31.16";
-            info.DeviceName = "惊艳PLUS";
-            info.DeviceAddr = 1;
-            //NetworkConnect connect = new NetworkConnect(info);
-            Thread.Sleep(300);
-            //Console.WriteLine("设备连接状态 : " + connect.IsConnected());
-            //connect.LightControlConnect(LCCCompleted, LCCError);
+          
         }
         public void Download(CCEntity entity)
         {
             
         }
-        private void KeyPressClickListener(Object obj)
+
+        public void BigDataTest(List<TongdaoWrapper> values)
         {
-            List<byte> data = obj as List<byte>;
-        }
-        private void LCCCompleted(Object obj,string msg)
-        {
-            Console.WriteLine("灯控设备连接成功\n" + msg);
-        }
-        private void KPCConpleted(Object obj)
-        {
-            Console.WriteLine("墙板设备连接成功");
-            Thread.Sleep(500);
-        }
-        private void KPRCompleted(Object obj)
-        {
-            KeyEntity entity = obj as KeyEntity;
-            Console.WriteLine("墙板设备读取成功");
-            //SerialConnect.KeyPressDownload(entity, KPDCompleted, KPDError);
-        }
-        private void LCCError(string msg)
-        {
-            Console.WriteLine("灯控设备连接失败[" + msg + "]");
-        }
-        private void KPCError()
-        {
-            Console.WriteLine("墙板设备连接失败");
-        }
-        private void KPRError()
-        {
-            Console.WriteLine("墙板设备读取失败");
-        }
-        private void KPDCompleted(Object obj)
-        {
-            Console.WriteLine("墙板设备下载数据成功");
-        }
-        private void KPDError()
-        {
-            Console.WriteLine("墙板设备下载数据失败");
+
         }
     }
 }

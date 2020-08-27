849731e86007a8b591ce4866cdbd2b9ee6315ed8
Dickov <fwm164@qq.com>
2020-08-27 10:34:05 +0800

    #修复825版本因为只渲染一次灯具的通道列表导致的bug：若一开始没有步数，则不生成该灯具的通道（现更改为所有灯具都进行渲染，把灯具是否显示放在_Load方法内）；#更新日志

--- LightController/MyForm/Multiplex/DetailMultiAstForm.cs
+++ LightController/MyForm/Multiplex/DetailMultiAstForm.cs
@@ -14,97 +14,104 @@ namespace LightController.MyForm.Multiplex
 	public partial class DetailMultiAstForm : Form
 	{
 		private MainFormBase mainForm;
-
+		
 		public DetailMultiAstForm(MainFormBase mainForm)
 		{
 			this.mainForm = mainForm;
 
 			InitializeComponent();
 
-			initAllPanels();
+			// 初始化时，所有的灯具都要生成相应面板（但显示与否由 load内决定）【这样之前的勾选可以保留】
+			for (int lightIndex = 0; lightIndex < mainForm.LightAstList.Count; lightIndex++)
+			{
+				IList<TongdaoWrapper> tongdaoList = mainForm.LightWrapperList[lightIndex].StepTemplate.TongdaoList;
+				LightAst la = mainForm.LightAstList[lightIndex];
+
+				Panel lightPanel = new Panel
+				{
+					Name = "lightPanel" + (lightIndex + 1),
+					Location = lightPanelDemo.Location,
+					Size = lightPanelDemo.Size,
+					BackColor = lightPanelDemo.BackColor
+				};
+
+				Label lightLabel = new Label
+				{
+					Name = "lightLabel" + (lightIndex + 1),
+					Text = la.LightType + "\n(" + la.LightAddr + ")",
+					Dock = lightLabelDemo.Dock,
+					Location = lightLabelDemo.Location,
+					Size = lightLabelDemo.Size,
+					TextAlign = lightLabelDemo.TextAlign,
+					Font = lightLabelDemo.Font,
+				};
+
+				CheckBox allCheckBox = new CheckBox
+				{
+					Name = "allCheckBox" + (lightIndex + 1),
+					AutoSize = allCheckBoxDemo.AutoSize,
+					Font = allCheckBoxDemo.Font,
+					Location = allCheckBoxDemo.Location,
+					Size = allCheckBoxDemo.Size,
+					Text = allCheckBoxDemo.Text,
+					UseVisualStyleBackColor = allCheckBoxDemo.UseVisualStyleBackColor
+				};
+				allCheckBox.CheckedChanged += allCheckBox_CheckedChanged;
+
+				FlowLayoutPanel lightFLP = new FlowLayoutPanel
+				{
+					Name = "lightFLP" + (lightIndex + 1),
+					AutoScroll = lightFLPDemo.AutoScroll,
+					Location = lightFLPDemo.Location,
+					Margin = lightFLPDemo.Margin,
+					Size = lightFLPDemo.Size
+				};
+
+				for (int tdIndex = 0; tdIndex < tongdaoList.Count; tdIndex++)
+				{
+					CheckBox tdCheckBox = new CheckBox
+					{
+						Name = "tdCheckBox" + (tdIndex + 1),
+						Text = tongdaoList[tdIndex].TongdaoName,
+						AutoSize = tdCheckBoxDemo.AutoSize,
+						Location = tdCheckBoxDemo.Location,
+						Size = tdCheckBoxDemo.Size,
+						UseVisualStyleBackColor = tdCheckBoxDemo.UseVisualStyleBackColor
+					};
+					lightFLP.Controls.Add(tdCheckBox);
+				}
+
+				lightPanel.Controls.Add(lightLabel);
+				lightPanel.Controls.Add(allCheckBox);
+				lightPanel.Controls.Add(lightFLP);
+
+				bigFlowLayoutPanel.Controls.Add(lightPanel);
+			}
 		}
 
+		/// <summary>
+		///  在load中进行窗体位置的更改；以及没有步数的灯具的隐藏
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void DetailMultiAstForm_Load(object sender, EventArgs e)
 		{
 			Location = new Point(mainForm.Location.X + 100, mainForm.Location.Y + 200);
-		}
-		
-		private void initAllPanels()
-		{
-			for(int lightIndex = 0; lightIndex<mainForm.LightAstList.Count; lightIndex++) 
-			{				
+
+			// 在load中决定哪些灯具要进行显示		
+			for (int lightIndex = 0; lightIndex < mainForm.LightAstList.Count; lightIndex++)
+			{
 				LightStepWrapper lsWrapper = mainForm.LightWrapperList[lightIndex].LightStepWrapperList[mainForm.CurrentFrame, mainForm.CurrentMode];
-
-				//如果存在部分灯具步数为空，则不执行之后的代码了
-				if (lsWrapper != null && lsWrapper.StepWrapperList !=null && lsWrapper.StepWrapperList.Count>0)
+				if (lsWrapper != null && lsWrapper.StepWrapperList != null && lsWrapper.StepWrapperList.Count > 0)
 				{
-					LightAst la = mainForm.LightAstList[lightIndex];
-					StepWrapper lightTemplate = mainForm.LightWrapperList[lightIndex].StepTemplate;
-					IList<TongdaoWrapper> tongdaoList = lightTemplate.TongdaoList;
-
-					Panel lightPanel = new Panel
-					{
-						Name = "lightPanel" + (lightIndex + 1),
-						Location = lightPanelDemo.Location,
-						Size = lightPanelDemo.Size,
-						BackColor = lightPanelDemo.BackColor
-					};
-
-					Label lightLabel = new Label
-					{
-						Name = "lightLabel" + (lightIndex + 1),
-						Text = la.LightType + "\n(" + la.LightAddr + ")",
-						Dock = lightLabelDemo.Dock,
-						Location = lightLabelDemo.Location,
-						Size = lightLabelDemo.Size,
-						TextAlign = lightLabelDemo.TextAlign,
-						Font = lightLabelDemo.Font,
-					};
-
-					CheckBox allCheckBox = new CheckBox
-					{
-						Name = "allCheckBox" + (lightIndex + 1),
-						AutoSize = allCheckBoxDemo.AutoSize,
-						Font = allCheckBoxDemo.Font,
-						Location = allCheckBoxDemo.Location,
-						Size = allCheckBoxDemo.Size,
-						Text = allCheckBoxDemo.Text,
-						UseVisualStyleBackColor = allCheckBoxDemo.UseVisualStyleBackColor
-					};
-					allCheckBox.CheckedChanged += allCheckBox_CheckedChanged;
-
-					FlowLayoutPanel lightFLP = new FlowLayoutPanel
-					{
-						Name = "lightFLP" + (lightIndex + 1),
-						AutoScroll = lightFLPDemo.AutoScroll,
-						Location = lightFLPDemo.Location,
-						Margin = lightFLPDemo.Margin,
-						Size = lightFLPDemo.Size
-					};
-
-					for (int tdIndex = 0; tdIndex < tongdaoList.Count; tdIndex++)
-					{
-						CheckBox tdCheckBox = new CheckBox
-						{
-							Name = "tdCheckBox" + (tdIndex + 1),
-							Text = tongdaoList[tdIndex].TongdaoName,
-							AutoSize = tdCheckBoxDemo.AutoSize,
-							Location = tdCheckBoxDemo.Location,
-							Size = tdCheckBoxDemo.Size,
-							UseVisualStyleBackColor = tdCheckBoxDemo.UseVisualStyleBackColor
-						};
-						lightFLP.Controls.Add(tdCheckBox);
-					}
-
-					lightPanel.Controls.Add(lightLabel);
-					lightPanel.Controls.Add(allCheckBox);
-					lightPanel.Controls.Add(lightFLP);
-
-					bigFlowLayoutPanel.Controls.Add(lightPanel);
+					bigFlowLayoutPanel.Controls[lightIndex + 1].Show();
+				}
+				else {
+					bigFlowLayoutPanel.Controls[lightIndex + 1].Hide() ; 
 				}
 			}
 		}
-	
+		
 		/// <summary>
 		/// 事件：勾选或去掉勾选allCheckBox复选框
 		/// </summary>
@@ -131,9 +138,9 @@ namespace LightController.MyForm.Multiplex
 			
 			foreach (Panel panel in bigFlowLayoutPanel.Controls)
 			{
-				if (panel.Visible) {
-					int lightIndex = MathHelper.GetIndexNum(panel.Name, -1);
-					Console.WriteLine("LI = " + lightIndex);
+				// 除了可以屏蔽panelDemo之外，刚好也屏蔽了其他步数为0的灯具；
+				if (panel.Visible) { 
+					int lightIndex = MathHelper.GetIndexNum(panel.Name, -1);					
 
 					FlowLayoutPanel lightFLP = panel.Controls[2] as FlowLayoutPanel;
 					List<int> tdIndices = new List<int>();

b9a262459606b8d2c374bd34bc688df45a4df0cb
Dickov <fwm164@qq.com>
2020-10-13 15:40:49 +0800

    #

---
+++ SpecialTools/Utils/MathHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightController.Common
+{
+	/// <summary>
+	/// 这是个数字与字符串等相关的辅助转换类，内置一些静态的方法，用来实现不同的目的
+	/// </summary>
+	public class MathHelper
+	{
+		/// <summary>
+		///  由输入的num，自动补零的两位数字符串
+		/// </summary>
+		/// <param name="num"></param>
+		/// <returns></returns>
+		public static string GetAddZeroStr(int num)
+		{
+			num = num + 1;
+			if (num < 10)
+			{
+				return "0" + num;
+			}
+			else
+			{
+				return num.ToString();
+			}
+		}
+
+		/// <summary>
+		///  辅助方法：将一个形如“单词串+数字”的组合，提取其数字值,再将这个数字减一，即可得到index值
+		/// 《LightEditor》 method：取出触发的sender的Name，对其进行操作
+		/// 1.替换掉非数字的字符串;2.将取出的数字-1；即可得到数组下标
+		/// </summary>
+		/// <param name="addNum">取出来的index需要加的数字，比如 label1 = labels[0] 则addNum = -1</param>
+		public static int GetIndexNum(String senderName,int addNum)
+		{
+			string indexStr = System.Text.RegularExpressions.Regex.Replace(senderName, @"[^0-9]+", "");
+			int numIndex = int.Parse(indexStr) + (addNum) ;
+			return numIndex;
+		}
+
+		/// <summary>
+		/// 辅助方法：将一个正整数变成占四个位置的字符串
+		/// </summary>
+		/// <param name="num">原整数</param>
+		/// <returns></returns>
+		public static string GetFourWidthNumStr(int num,bool insertBefore ) {
+			string result = "";
+			if (num < 10) {
+				if (insertBefore)
+				{
+					result = " " + num + "  ";
+				}
+				else
+				{
+					result = "  " + num + " ";
+				}
+			} else if (num >= 10 && num < 100) {
+				result = " " + num + " ";
+			} else if (num >= 100 && num < 1000)
+			{
+				if (insertBefore)
+				{
+					result = num + " ";
+				}
+				else {
+					result = " " + num ;
+				}				
+			} else {
+				result = "" + num;
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// 辅助方法：传入两个int，将其相除后的小数向上取整并返回
+		/// </summary>
+		/// <param name="numA"></param>
+		/// <param name="numB"></param>
+		/// <returns></returns>
+		public static int GetDividionCelling(int numA, int numB) {
+			decimal r1 = (decimal)numA / numB; //先相除，得到精确的decimal
+			decimal r2 = Math.Ceiling(r1); //向上取整
+			int r3 = decimal.ToInt32(r2); //转为int
+			return r3;
+		}
+
+	}
+}
--- SpecialTools/specialTDForm.cs
+++ SpecialTools/specialTDForm.cs
@@ -1,4 +1,5 @@
-﻿using LightController.Utils.LightConfig;
+﻿using LightController.Common;
+using LightController.Utils.LightConfig;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,13 +11,103 @@ using System.Windows.Forms;
 
 namespace SpecialTools
 {
-	public partial class specialTDForm : Form
+	public partial class SpecialTDForm : Form
 	{
+		private IList<string> frameList;
 		private int[] tdArray;
+		private LightConfigBean lcb;
+		private int frameCount = 32;
+		private int tdCount = 10;
 
-		public specialTDForm()
+		private Panel[] tdPanels;
+		private Label[] tdLabels;
+		private CheckBox[] tdCBs;
+		private NumericUpDown[] tdNUDs;
+
+		public SpecialTDForm()
 		{
 			InitializeComponent();
+					   
+			// 初始化场景选择
+			frameList = TextHelper.Read(Application.StartupPath + @"\FrameList.txt");
+			foreach (string frame in frameList)
+			{
+				frameComboBox.Items.Add(frame);
+			}
+			frameComboBox.SelectedIndexChanged -= frameComboBox_SelectedIndexChanged;
+			frameComboBox.SelectedIndex = 0;
+			frameComboBox.SelectedIndexChanged += frameComboBox_SelectedIndexChanged;
+
+
+			// 渲染FLP
+			tdPanels = new Panel[tdCount];
+			tdLabels = new Label[tdCount];
+			tdCBs = new CheckBox[tdCount];
+			tdNUDs = new NumericUpDown[tdCount];
+
+			for (int tdIndex = 0; tdIndex < tdCount; tdIndex++)
+			{
+				tdPanels[tdIndex] = new Panel
+				{
+					Name = "tdPanel" + (tdIndex + 1),
+					Size = tdPanelDemo.Size,
+					BorderStyle = tdPanelDemo.BorderStyle,
+					Visible = false
+				};
+
+				tdLabels[tdIndex]= new Label
+				{
+					Name = "tdLabel" + (tdIndex + 1),
+					AutoSize = tdLabelDemo.AutoSize,
+					Location = tdLabelDemo.Location,
+					Size = tdLabelDemo.Size,
+				};
+
+				tdCBs[tdIndex] = new CheckBox
+				{
+					Name = "tdCB" + (tdIndex + 1),
+					Text = tdCBDemo.Text,
+					AutoSize = tdCBDemo.AutoSize,
+					Location = tdCBDemo.Location,
+					Size = tdCBDemo.Size,
+					UseVisualStyleBackColor = tdCBDemo.UseVisualStyleBackColor,
+				};
+
+				tdNUDs[tdIndex] = new NumericUpDown
+				{
+					Name = "tdNUD" + (tdIndex + 1),
+					Location = tdNUDDemo.Location,
+					Size = tdNUDDemo.Size,
+					TextAlign = tdNUDDemo.TextAlign,
+					Maximum = tdNUDDemo.Maximum,
+				};
+				tdNUDs[tdIndex].MouseWheel += someNUD_MouseWheel;
+
+				tdPanels[tdIndex].Controls.Add(tdLabels[tdIndex]);
+				tdPanels[tdIndex].Controls.Add(tdCBs[tdIndex]);
+				tdPanels[tdIndex].Controls.Add(tdNUDs[tdIndex]);
+
+				tdFLP.Controls.Add(tdPanels[tdIndex]);
+			}
+		}
+		
+		/// <summary>
+		/// 事件：加载界面时执行代码
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void specialTDForm_Load(object sender, EventArgs e)
+		{
+		}
+
+		/// <summary>
+		/// 事件：更改场景
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void frameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			generateFLP();
 		}
 
 		/// <summary>
@@ -28,14 +119,14 @@ namespace SpecialTools
 		{
 			string tdStr = tdTextBox.Text.Trim();
 			string[] tdStrArray = tdStr.Split(' ');	
-			if (tdStrArray.Length != 10) {
-				setNotice("通道数量非十个,请重新填写！",true);
+			if (tdStrArray.Length != tdCount) {
+				setNotice("通道数量非"+ tdCount + "个,请重新填写！",true);
 				return;
 			}
 			try
 			{
-				tdArray = new int[10];
-				for (int tdIndex = 0; tdIndex < 10; tdIndex++)
+				tdArray = new int[tdCount];
+				for (int tdIndex = 0; tdIndex < tdCount; tdIndex++)
 				{
 					tdArray[tdIndex] = int.Parse(tdStrArray[tdIndex]);
 				}				
@@ -44,17 +135,19 @@ namespace SpecialTools
 				setNotice("请填入正确的通道数，不得填入非数字!",true);
 				return;
 			}
+
+			generateEmptyLCB(); 
 			generateFLP();
 		}
 
 		/// <summary>
-		/// 根据通道列表，生成相应的面板；
+		///  辅助方法：生成lcb对象
 		/// </summary>
-		private void generateFLP()
-		{
-			LightConfigBean lcb = new LightConfigBean();
+		private void generateEmptyLCB() {
+
+			lcb = new LightConfigBean();
 			lcb.SceneConfigs = new SceneConfigBean[32];
-			for (int scIndex= 0;   scIndex<lcb.SceneConfigs.Length; scIndex++)
+			for (int scIndex = 0; scIndex < lcb.SceneConfigs.Length; scIndex++)
 			{
 				lcb.SceneConfigs[scIndex] = new SceneConfigBean();
 				lcb.SceneConfigs[scIndex].ChannelConfigs = new ChannelConfigBean[10];
@@ -68,58 +161,46 @@ namespace SpecialTools
 					};
 				}
 			}
+		}
+			   
+		/// <summary>
+		/// 根据通道列表，生成相应的面板；
+		/// </summary>
+		private void generateFLP()
+		{
+			if (lcb == null) {
+				setNotice("尚未生成lcb，无法渲染FLP", true);
+				return;
+			}
 
-			LightConfigBean.WriteToFile(  @"C:\lightConfig.bin" , lcb);
+			int frame = frameComboBox.SelectedIndex;
+			for (int tdIndex = 0; tdIndex < tdCount; tdIndex++)
+			{
+				tdPanels[tdIndex].Visible = true ;
+				tdLabels[tdIndex].Text = "通道" + lcb.SceneConfigs[frame].ChannelConfigs[tdIndex].ChannelNo;
 
-			//tdFLP.Controls.Clear();//先清空
-			//for (int tdIndex = 0; tdIndex < 10; tdIndex++)
-			//{
-			//	Panel tdPanel = new Panel
-			//	{
-			//		Name = "tdPanel" +(tdIndex+1),
-			//		Size = tdPanelDemo.Size,
-			//		BorderStyle = tdPanelDemo.BorderStyle,
-			//		Visible = true
-			//	};
+				tdCBs[tdIndex].CheckedChanged -= tdCB_CheckedChanged;
+				tdCBs[tdIndex].Checked = lcb.SceneConfigs[frame].ChannelConfigs[tdIndex].IsOpen;
+				tdCBs[tdIndex].CheckedChanged += tdCB_CheckedChanged;
 
-			//	Label tdLabel = new Label
-			//	{
-			//		Name = "tdLabel" + (tdIndex + 1),
-			//		Text = "通道" + tdArray[tdIndex] ,
-			//		AutoSize = tdLabelDemo.AutoSize,
-			//		Location = tdLabelDemo.Location,					
-			//		Size = tdLabelDemo.Size,
-			//	};
-
-			//	CheckBox tdCB = new CheckBox
-			//	{
-			//		Name = "tdCB" + (tdIndex + 1),
-			//		Text = tdCBDemo.Text ,
-			//		AutoSize = tdCBDemo.AutoSize,
-			//		Location = tdCBDemo.Location,
-			//		Size = tdCBDemo.Size,					
-			//		UseVisualStyleBackColor = tdCBDemo.UseVisualStyleBackColor,
-			//	};			
-
-			//	NumericUpDown tdNUD = new NumericUpDown
-			//	{
-			//		Name = "tdNUD" + (tdIndex + 1),
-			//		Location = tdNUDDemo.Location,
-			//		Size = tdNUDDemo.Size,
-			//		TextAlign = tdNUDDemo.TextAlign,
-			//	};
-
-			//	tdPanel.Controls.Add( tdLabel );
-			//	tdPanel.Controls.Add( tdCB );
-			//	tdPanel.Controls.Add( tdNUD );
-
-			//	tdFLP.Controls.Add(tdPanel);
-			//}
+				tdNUDs[tdIndex].ValueChanged -= tdNUD_ValueChanged;
+				tdNUDs[tdIndex].Value = lcb.SceneConfigs[frame].ChannelConfigs[tdIndex].DefaultValue;
+				tdNUDs[tdIndex].ValueChanged += tdNUD_ValueChanged;
+			}
 		}
 
-		private void saveButton_Click(object sender, EventArgs e)
+		/// <summary>
+		/// 事件：点击《导出文件》
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void exportButton_Click(object sender, EventArgs e)
 		{
-
+			if (lcb == null) {
+				setNotice( "尚未生成lcb对象，无法导出文件。" ,  true);
+				return;
+			}
+			LightConfigBean.WriteToFile(@"C:\lightConfig.bin", lcb);
 		}
 
 		/// <summary>
@@ -133,6 +214,66 @@ namespace SpecialTools
 				MessageBox.Show(msg);
 			}
 		}
+
+		/// <summary>
+		/// 事件：通道是否启用变更后，更改lcb内的相应值
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void tdCB_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// 事件：通道默认值更改后，更改lcb内相应值
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void tdNUD_ValueChanged(object sender, EventArgs e)
+		{
+			if (lcb != null) {
+				NumericUpDown nud = sender as NumericUpDown;
+				int tdIndex = MathHelper.GetIndexNum(nud.Name, -1);
+				int frameIndex = frameComboBox.SelectedIndex;
+
+				//lcb.SceneConfigs[frameIndex].ChannelConfigs[tdIndex]
+			}
+		}
+
+		/// <summary>
+		/// 验证：对某些NumericUpDown进行鼠标滚轮的验证，避免一次性滚动过多
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void someNUD_MouseWheel(object sender, MouseEventArgs e)
+		{
+			NumericUpDown nud = sender as NumericUpDown;
+			HandledMouseEventArgs hme = e as HandledMouseEventArgs;
+			if (hme != null)
+			{
+				hme.Handled = true;
+			}
+			// 向上滚
+			if (e.Delta > 0)
+			{
+				decimal dd = nud.Value + nud.Increment;
+				if (dd <= nud.Maximum)
+				{
+					nud.Value = decimal.ToInt32(dd);
+				}
+			}
+			// 向下滚
+			else if (e.Delta < 0)
+			{
+				decimal dd = nud.Value - nud.Increment;
+				if (dd >= nud.Minimum)
+				{
+					nud.Value = decimal.ToInt32(dd);
+				}
+			}
+		}
+
 
 	}
 }

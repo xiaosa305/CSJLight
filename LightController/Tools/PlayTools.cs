59a8b56be6cd9d0f6c3a18e5d19480137829b7fe
维佳 曾 <997295205@qq.com>
2021-08-23 11:21:13 +0800

    ####关闭调试发送状态前移

--- LightController/Tools/PlayTools.cs
+++ LightController/Tools/PlayTools.cs
@@ -106,8 +106,8 @@ namespace LightController.Tools
         }
         public void StopPreview(Completed_TakeMsgAndObj completed,Error error)
         {
-            this.Communication.StopIntentPreview(completed, error);
             this.DebugStatus = false;
+            this.Communication.StopIntentPreview(completed, error);
         }
 
         private void Compelted(Object obj,string msg)

46fd2adc353e41edc56097b9109ef13548a57d56
Dickov <fwm164@qq.com>
2021-10-13 09:14:09 +0800

    #

--- LightController/MyForm/MainFormInterface.cs
+++ LightController/MyForm/MainFormInterface.cs
@@ -8,7 +8,7 @@ namespace LightController.MyForm
 {
 	public interface MainFormInterface
 	{
-		//DOTO 要重写 GetFMTDList
+		//DOTO 211012 要重写 GetFMTDList
 		//MARK 只开单场景：10.0 抽象出GetFMTDList() ，主要供维佳调用（放在MainFormInterface中）
 		IList<TongdaoWrapper> GetFMTDList(/*DB_ValuePK pk*/);
 	}
